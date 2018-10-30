b3fa622bbe9da74b8d46a17cdee04e99eabb7e6a
Gavin <31890432+liffera17@users.noreply.github.com>
2018-10-30 18:38:27 +0700

    Update views/login2.php

---
+++ views/login2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Pendaftaran</title>
+	<link rel="stylesheet" href="./assets/css/bootstrap.css">
+	<style>
+	body {
+		margin-top: 40px;
+	}
+</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<h1>login bodo</h1>			
+		</div>
+	</div>
+</body>
+</html>
